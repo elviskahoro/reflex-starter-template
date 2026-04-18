5594fb8e6400d974f513a1bc1af4556f0795c718
elvis kahoro <github@elvis.ai>
2026-04-18 09:53:01 -0700

    fix: handle /ping health check in Caddy instead of Reflex

--- web/web.py
+++ web/web.py
@@ -4,11 +4,6 @@ import reflex as rx
 
 from web.pages.index import page
 
-
-async def ping_handler(request):
-    return rx.response.Response(content="pong", status_code=200)
-
-
 app = rx.App()
 app.add_page(
     component=page,
@@ -23,5 +18,3 @@ app.add_page(
         },
     ],
 )
-
-app.add_route("/ping", ping_handler, methods=["GET"])
